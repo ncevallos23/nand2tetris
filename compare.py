79d7f7a4d658b1a787d9c6f068dc50b1cf6df8ff
ncevallos23 <68822026+ncevallos23@users.noreply.github.com>
2022-01-26 14:28:43 -0700

    fixed somethings on compare and CPU

--- compare.py
+++ compare.py
@@ -13,9 +13,25 @@ f2 = open(file2, "r")
 for line in f2:
     file2_array.append(line)
 
+print(file1_array[0].strip())
+
+compare = []
+compare_num = []
+
+first = True
+
 for line_num in range(0, len(file1_array)):
-    if file1_array[line_num] != file2_array[line_num]:
-        print("Comparison failed at line " + str(line_num) + ":")
-        print(file1_array[line_num])
-        print(file2_array[line_num])
-        print("")
+    for char_num in range(0, len(file1_array[line_num])):
+        if file1_array[line_num][char_num] == "*" or file2_array[line_num][char_num] == "*":
+            continue
+        if file1_array[line_num][char_num] != file2_array[line_num][char_num]:
+            if first:
+                compare.append(["line " + str(line_num) + ":", file1_array[line_num-1].strip(), file2_array[line_num-1].strip(), ""])
+                first = False
+            compare.append(["Comparison failed at line " + str(line_num+1) + ":", file1_array[line_num].strip(), file2_array[line_num].strip(), ""])
+            compare_num.append(line_num)
+            break
+
+for state in compare:
+    for a in state:
+        print(a)
