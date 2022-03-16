e0e44a2a371881bb2a68eccc2a40fb5494400a18
ncevallos23 <68822026+ncevallos23@users.noreply.github.com>
2022-03-16 12:28:31 -0700

    fisnihed CompilationEngine.CompileClass()

--- projects/10/compiler/CompilationEngine.py
+++ projects/10/compiler/CompilationEngine.py
@@ -1,12 +1,37 @@
 import JackTokenizer
+import sys
+
+keywordSub = ['constructor', 'function', 'method']
+keywordClassVarDec = ['field', 'static']
 
 class CompilationEngine:
     def __init__(self, token_in, file_out):
         self.tokenizer_in = token_in #this is a JackTokenizer class
-        self.file_output = file_out
+        self.output = []
+        self.out_file = file_out
     
     def CompileClass(self):
-        pass
+        self.output.append('<class>')
+        class_check = self.tokenizer_in.lookAhead(2, True)
+        if class_check[1][1] == 'identifier' and class_check[2][0] == '{':
+            self.output.append('<'+class_check[0][1]+'>'+' '+class_check[0][0]+' '+'</'+class_check[0][1]+'>')
+            self.output.append('<'+class_check[1][1]+'>'+' '+class_check[1][0]+' '+'</'+class_check[1][1]+'>')
+            self.output.append('<'+class_check[2][1]+'>'+' '+class_check[2][0]+' '+'</'+class_check[2][1]+'>')
+        else:
+            print("error with class declaration")
+            print(class_check[0][0] + ' ' + class_check[1][0] + ' ' + class_check[2][0])
+            sys.exit()
+        while(self.tokenizer_in.hasMoreTokens()):
+            self.tokenizer_in.advance()
+            current = self.tokenizer_in.getToken()
+            if current[0] in keywordClassVarDec:
+                self.CompileClassVarDec()
+            elif current[0] in keywordSub:
+                self.CompileSubroutine()
+            else:
+                print("error with class var dec or subroutine dec")
+                sys.exit()
+        self.output.append('</class>')
 
     def CompileClassVarDec(self):
         pass
